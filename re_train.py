9516843de65a6d64b0ebcfc91029d22355e11c36
ys <lord.of.the.red@gmail.com>
2017-01-31 17:46:11 +0900

    20170131: try to make difference between cathe->blue and organ->red...

--- re_train.py
+++ re_train.py
@@ -17,9 +17,6 @@ from chainer import cuda,Variable,optimizers
 import time
 import cupy
 
-
-
-
 # 引数の処理
 parser = argparse.ArgumentParser(
     description='train convolution filters')
@@ -30,17 +27,13 @@ class Conv(chainer.Chain):
 	def __init__(self):
 		super(Conv, self).__init__(
 			# 入力・出力1ch, ksize=3
-			conv1=F.Convolution2D(1, 64, 5, pad=2),#conv1=F.Convolution2D(1, 32, 3, pad=1),
-			conv2=F.Convolution2D(64, 1, 5, pad=2),
-			conv3=F.Convolution2D(1, 64, 5, pad=2),#conv1=F.Convolution2D(1, 32, 3, pad=1),
-			conv4=F.Convolution2D(64, 1, 5, pad=2),
-			conv5=F.Convolution2D(1, 64, 5, pad=2),#conv1=F.Convolution2D(1, 32, 3, pad=1),
-			conv6=F.Convolution2D(64, 1, 5, pad=2),
-			conv7=F.Convolution2D(1, 64, 3, pad=1),#conv1=F.Convolution2D(1, 32, 3, pad=1),
-			conv8=F.Convolution2D(64, 1, 3, pad=1),
-			conv9=F.Convolution2D(1, 64, 5, pad=2),#conv1=F.Convolution2D(1, 32, 3, pad=1),
-			conv10=F.Convolution2D(64, 1, 5, pad=2),
-norm1=L.BatchNormalization(1),
+			conv3_32=F.Convolution2D(3, 32, 5, pad=2),#conv1=F.Convolution2D(1, 32, 3, pad=1),
+			conv32_3=F.Convolution2D(32, 3, 5, pad=2),
+			conv3=F.Convolution2D(3, 64, 5, pad=2),#conv1=F.Convolution2D(1, 32, 3, pad=1),
+			conv4=F.Convolution2D(64, 3, 5, pad=2),
+			conv7=F.Convolution2D(3, 64, 3, pad=1),#conv1=F.Convolution2D(1, 32, 3, pad=1),
+			conv8=F.Convolution2D(64, 3, 3, pad=1),
+			norm_ch3=L.BatchNormalization(3),
 		)
 
 	def clear(self):
@@ -49,63 +42,67 @@ norm1=L.BatchNormalization(1),
 
 	def forward(self, x,layer,train=True):
 		self.clear()
+
+		#print(x.data.shape)
+		h = F.relu(model.conv3_32(x))
+		h = F.relu(model.conv32_3(h))
+		h = F.relu(self.norm_ch3(h,test= not train))
 		
-		h = F.relu(model.conv1(x))
-		h = F.relu(model.conv2(h))
-		h = F.relu(self.norm1(h,test= not train))
-		
-		h = F.relu(model.conv3(h))
-		h = F.relu(model.conv4(h))
-		h = F.relu(self.norm1(h,test= not train))
+		h = F.relu(model.conv3_32(h))
+		h = F.relu(model.conv32_3(h))
+		h = F.relu(self.norm_ch3(h,test= not train))
 		
 		if layer > 0:
-			h = F.relu(model.conv5(h))
-			h = F.relu(model.conv6(h))
-			h = F.relu(self.norm1(h,test= not train))
+			h = F.relu(model.conv3_32(h))
+			h = F.relu(model.conv32_3(h))
+			h = F.relu(self.norm_ch3(h,test= not train))
 		if layer > 1:
-			h = F.relu(model.conv7(h))
-			h = F.relu(model.conv8(h))
-			h = F.relu(self.norm1(h,test= not train))
+			h = F.relu(model.conv3_32(h))
+			h = F.relu(model.conv32_3(h))
+			h = F.relu(self.norm_ch3(h,test= not train))
 		if layer > 2:
-			h = F.relu(model.conv9(h))
-			h = F.relu(model.conv10(h))
+			h = F.relu(model.conv3_32(h))
+			h = F.relu(model.conv32_3(h))
 		
 		return h
 
 	def calc_loss(self, x, t,layer,train=True):
 		self.clear()
 
-		h = F.relu(model.conv1(x))
-		h = F.relu(model.conv2(h))
-		h = F.relu(self.norm1(h,test= not train))
-		
-		h = F.relu(model.conv3(h))
-		h = F.relu(model.conv4(h))
-		h = F.relu(self.norm1(h,test= not train))
-		
+		#print(x.data.shape)
+		h = F.relu(model.conv3_32(x))
+		h = F.relu(model.conv32_3(h))
+		h = F.relu(self.norm_ch3(h, test=not train))
+
+		h = F.relu(model.conv3_32(h))
+		h = F.relu(model.conv32_3(h))
+		h = F.relu(self.norm_ch3(h, test=not train))
+
 		if layer > 0:
-			h = F.relu(model.conv5(h))
-			h = F.relu(model.conv6(h))
-			h = F.relu(self.norm1(h,test= not train))
+			h = F.relu(model.conv3_32(h))
+			h = F.relu(model.conv32_3(h))
+			h = F.relu(self.norm_ch3(h, test=not train))
 		if layer > 1:
-			h = F.relu(model.conv7(h))
-			h = F.relu(model.conv8(h))
-			h = F.relu(self.norm1(h,test= not train))
+			h = F.relu(model.conv3_32(h))
+			h = F.relu(model.conv32_3(h))
+			h = F.relu(self.norm_ch3(h, test=not train))
 		if layer > 2:
-			h = F.relu(model.conv9(h))
-			h = F.relu(model.conv10(h))
+			h = F.relu(model.conv3_32(h))
+			h = F.relu(model.conv32_3(h))
 			
 		loss = F.mean_squared_error(h, t)
 		return loss
 
 
-for layer in range(4):
+for layer in range(3):
 
 	#Root file
-	Ans_PATH= "IMG2BMP/160914172223.IMG/Ans"
-	Training_PATH= "IMG2BMP/160914172223.IMG/forTraining"
-	Result_PATH= "161101_"+str(layer)+"/"
-	
+	#Training_PATH= "/home/ys/Share/7_DL_model_set/ver20170123/15B24/DL_Ans_half"
+
+	Ans_PATH= "/home/ys/Share/7_DL_model_set/ver20170123/15B24/DL_Ans_half"
+	Training_PATH= "/home/ys/Share/7_DL_model_set/ver20170123/15B24/trn_half"
+	Result_PATH= "/home/ys/Share/15_NN_calc_result/170130_"+str(layer)+"/"
+	model_name ="170130.chainermodel"
 	if os.path.isdir(Result_PATH)==False:
 		os.mkdir(Result_PATH)
 
@@ -137,10 +134,14 @@ for layer in range(4):
 	for seq in range(2500):
 		filenames= random.sample(Ansfiles,100)
 		for filename in filenames:
-	#		print(filename)
-			train_image = chainer.Variable(cuda.cupy.asarray([[cv2.imread(Training_PATH+"/"+filename, 0)/255.0]], dtype=np.float32))
-			target = chainer.Variable(cuda.cupy.asarray([[cv2.imread(Ans_PATH+"/"+filename, 0)/255.0]], dtype=np.float32))
-		
+			#print(filename)
+			train_image = cv2.imread(Training_PATH+"/"+filename, cv2.IMREAD_COLOR)
+			train_image = train_image.transpose(2,0,1)
+			train_image = chainer.Variable(cuda.cupy.asarray([(train_image)/255.0], dtype=np.float32))
+
+			target = cv2.imread(Ans_PATH+"/"+filename, cv2.IMREAD_COLOR)
+			target = chainer.Variable(cuda.cupy.asarray([target.transpose(2,0,1)/255.0], dtype=np.float32))
+
 			loss = model.calc_loss(train_image, target, layer)
 			model.zerograds()
 			loss.backward()
@@ -163,23 +164,29 @@ for layer in range(4):
 			if os.path.isdir(Result_PATH+str(seq))==False:
 				os.mkdir(Result_PATH+str(seq))
 			for filename in Ansfiles:
-				train_image = chainer.Variable(cuda.cupy.asarray([[cv2.imread(Training_PATH +"/"+filename, 0)/255.0]], dtype=np.float32))
+				#train_image = chainer.Variable(cuda.cupy.asarray([[cv2.imread(Training_PATH +"/"+filename, 0)/255.0]], dtype=np.float32))
+				train_image = cv2.imread(Ans_PATH + "/" + filename, cv2.IMREAD_COLOR)
+				train_image = chainer.Variable(cuda.cupy.asarray([train_image.transpose(2, 0, 1) / 255.0], dtype=np.float32))
+				#print(train_image.data.shape)
 				trained = model.forward(train_image,layer).data[0][0]*255
 				cv2.imwrite(Result_PATH+str(seq)+"/"+filename, cuda.to_cpu(trained))
-			chainer.serializers.save_hdf5(Result_PATH+str(seq)+"/161101.model", model)
+				#cuda.to_cpu(trained).shape
+			chainer.serializers.save_hdf5(Result_PATH+str(seq)+"/"+model_name, model)
 	#	print(model.conv1.W.data[0][0])
 	#	trained = model.forward(train_image).data[0][0]*255
 	#	cv2.imwrite("trained.jpg", trained)
 
 	# 学習結果の表示
-	print(model.conv1.W.data[0][0])
+	#print(model.conv1.W.data[0][0])
 
 	if os.path.isdir(Result_PATH+str(seq))==False:
 		os.mkdir(Result_PATH+str(seq))
 		
 	for filename in Ansfiles:
-		train_image = chainer.Variable(cuda.cupy.asarray([[cv2.imread(Training_PATH +"/"+filename, 0)/255.0]], dtype=np.float32))
+		#train_image = chainer.Variable(cuda.cupy.asarray([[cv2.imread(Training_PATH +"/"+filename, 0)/255.0]], dtype=np.float32))
+		train_image = cv2.imread(Ans_PATH + "/" + filename, cv2.IMREAD_COLOR)
+		train_image = chainer.Variable(cuda.cupy.asarray([train_image.transpose(2, 0, 1) / 255.0], dtype=np.float32))
 		trained = model.forward(train_image,layer).data[0][0]*255
 		cv2.imwrite(Result_PATH+str(seq)+"/"+filename, cuda.to_cpu(trained))
 	
-	chainer.serializers.save_hdf5(Result_PATH+str(seq)+"/161101.model", model)
+	chainer.serializers.save_hdf5(Result_PATH+str(seq)+"/"+model_name, model)
