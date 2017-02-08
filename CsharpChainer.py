7b8f6802d048c95d4d306a6793c75bedddc20ad4
sakamoto <lord.of.the.red@gmail.com>
2017-02-08 21:32:49 +0900

    CsharpChainer.py において、multiprocessingを実装。 画像をフォルダから読み込み、読み込んだ画像を違うフォルダにNeural networkを通過した後の画像を保存するように設定。

--- CsharpChainer.py
+++ CsharpChainer.py
@@ -10,12 +10,16 @@ import chainer
 from chainer import optimizers
 import chainer.functions as F
 import chainer.links as L
-import random
 import numpy as np
+import multiprocessing
+from multiprocessing import Queue
+
 from chainer import cuda, Variable, optimizers
 import time
 import cupy
 from PIL import Image
+import shutil
+import Utility
 # 引数の処理
 parser = argparse.ArgumentParser(
     description='train convolution filters')
@@ -24,7 +28,13 @@ parser.add_argument('org', help='Path to original image')
 
 # クラスの定義
 class Conv(chainer.Chain):
-    def __init__(self):
+    # def __init__(self):# コンストラクタ
+    def __init__(self, Path, ResultPath):  # コンストラクタ
+        self.Path = Path
+        self.ResultPath = ResultPath
+        self.queue = Queue()
+        self.list = []
+
         super(Conv, self).__init__(
             # 入力・出力1ch, ksize=3
             conv3_32=F.Convolution2D(3, 32, 5, pad=2),  # conv1=F.Convolution2D(1, 32, 3, pad=1),
@@ -36,20 +46,24 @@ class Conv(chainer.Chain):
             norm_ch3=L.BatchNormalization(3),
         )
 
+
     def clear(self):
         self.loss = None
         self.accuracy = None
 
+    '''------------------------------------
+    Network Construction
+    ------------------------------------'''
     def forward(self, x, train=True):
         self.clear()
 
         # print(x.data.shape)
-        h = F.relu(model.conv3_32(x))
-        h = F.relu(model.conv32_3(h))
+        h = F.relu(self.conv3_32(x))
+        h = F.relu(self.conv32_3(h))
         h = F.relu(self.norm_ch3(h, test=not train))
 
-        h = F.relu(model.conv3_32(h))
-        h = F.relu(model.conv32_3(h))
+        h = F.relu(self.conv3_32(h))
+        h = F.relu(self.conv32_3(h))
         h = F.relu(self.norm_ch3(h, test=not train))
 
         '''
@@ -68,41 +82,97 @@ class Conv(chainer.Chain):
         '''
         return h
 
+    '''------------------------------------
+    Network processing
+    ------------------------------------'''
+    def RunNetwork(self, filename, Path, ResultPath):
 
-# Root file
-# Training_PATH= "/home/ys/Share/7_DL_model_set/ver20170123/15B24/DL_Ans_half"
-img_PATH = r"C:\Users\Sakamoto\Source\Repos\XY2RT\XY2RY\SoundFlow\bin\Debug\beatjudgement"
-filename = r"17.bmp"
-Result_PATH = r"C:\Users\Sakamoto\Source\Repos\XY2RT\XY2RY\SoundFlow\bin\Debug\DLresult"
-model_name = r"C:\Users\Sakamoto\Source\Repos\XY2RT\XY2RY\SoundFlow\bin\Debug\170130_0.chainermodel"
+        cv_image = cv2.imread(Path  + r"\\" + filename, cv2.IMREAD_COLOR)
+        orgHeight, orgWidth = cv_image .shape[:2]
+        cv_image = cv2.resize(cv_image, (orgHeight//2, orgWidth//2))
 
-# 学習対象のモデル作成
-model = Conv()
-chainer.serializers.load_hdf5(model_name,model)
-#chainer.cuda.get_device(0).use()  # Make a specified GPU current
-model.to_cpu()  # Copy the model to the GPU
+        ## 最後のtranseposeは RGBRGBRGB ----> RRRRGGGGBBBって感じに変換
+        train_image = chainer.Variable(np.asarray([cv_image.transpose(2, 0, 1) / 255.0], dtype=np.float32))
+        trained = self.forward(train_image)  # calculate network
 
-cv_image = cv2.imread(img_PATH + r"\\"+ filename, cv2.IMREAD_COLOR)
-print (cv_image.shape)
-## 最後のtranseposeは RGBRGBRGB ----> RRRRGGGGBBBって感じに変換
-train_image = chainer.Variable(np.asarray([cv_image.transpose(2, 0, 1) / 255.0], dtype=np.float32))
-trained = model.forward(train_image)#.data[0][0] * 255
-'''
-print ("train_image.size")
-print (train_image.size)
-print ("trained.size")
-print (trained.size)
-print ("trained.data.size")
-print (trained.data.size)
-#test = trained.reshape(1,3,300,256)
-print ("modi trained.size")
-print (trained.size)
-'''
-im = np.zeros((300,256,3))
-im[:,:,0] = trained.data[0][0]*255
-im[:,:,1] = trained.data[0][1]*255
-im[:,:,2] = trained.data[0][2]*255
-resized_img = cv2.resize(im, (256,300))
-#im =im.transpose(0,2,3,1)
-#Image.fromarray(im).save(Result_PATH +  r"\\"+ filename)
-cv2.imwrite(Result_PATH +  r"\\"+ filename, resized_img)
+        im = np.zeros((300, 256, 3))
+        im[:, :, 0] = trained.data[0][0] * 255
+        im[:, :, 1] = trained.data[0][1] * 255
+        im[:, :, 2] = trained.data[0][2] * 255
+        resized_img = cv2.resize(im, (256, 300))
+
+        cv2.imwrite(ResultPath + r"\\" + filename, resized_img)
+
+    '''------------------------------------
+    multithread processing -Read directory-
+    ------------------------------------'''
+    def Step1(self):
+        i = 0
+        print("start1 started i = ", str(i))
+        os.mkdir(self.Path+ r"\\tmp_for_dl")
+
+        while True:
+            filelist = Utility.check_file(self.Path, ".bmp")
+
+            if len(filelist) != 0:#if there is no file in the directory
+                for file in filelist:
+                    if os.path.exists(self.Path + r"\\" + file):
+                        shutil.move(self.Path+ r"\\" + file, self.Path+ r"\\tmp_for_dl")
+                        self.queue.put(file)
+
+            else:
+                i = i + 1
+                print("step1 i=", str(i))
+                if i > 10:
+                    print("step1 break")
+                    break
+
+    '''------------------------------------
+    multithread processing -Calculate Deep Learning-
+    ------------------------------------'''
+    def Step2(self):
+        i = 0
+        print("step2 started")
+        start = time.time()
+
+        while True:
+            if self.queue.empty():
+                print("step2 no queue")
+                i = i + 1
+                if i == 10:
+                    print("step2 break")
+                    os.rmdir(self.Path+ r"\\tmp_for_dl")
+                    break
+            else:
+                print("step2:", self.queue.qsize())
+                tmp_file = self.queue.get()
+                if os.path.exists(self.Path+ r"\\tmp_for_dl\\" + tmp_file):
+                    #run Neural network
+                    self.RunNetwork(tmp_file, self.Path+ r"\\tmp_for_dl", self.ResultPath)
+                    os.remove(self.Path+ r"\\tmp_for_dl\\" + tmp_file)
+                elapsed_time = time.time() - start
+                print(("run NN elapsed_time:{0}".format(elapsed_time)), "[sec]")
+
+
+
+if __name__ == "__main__":
+    model_name = r"C:\Users\Sakamoto\Source\Repos\XY2RT\XY2RY\SoundFlow\bin\Debug\170130_0.chainermodel"
+    Path = r"C:\Users\Sakamoto\PycharmProjects\Chainer\sample"
+    Result_PATH = r"C:\Users\Sakamoto\PycharmProjects\Chainer\sample2"
+    start = time.time()
+    # 既存のモデルを用いて、ネットワーク拡大
+    model = Conv(Path, Result_PATH)
+    chainer.serializers.load_hdf5(model_name, model)
+    model.to_cpu() # Copy the model to the CPU
+
+    elapsed_time = time.time() - start
+    print(("elapsed_time:{0}".format(elapsed_time)) , "[sec]")
+
+    t1 = multiprocessing.Process(target=model.Step1)
+    t2 = multiprocessing.Process(target=model.Step2)
+
+    t1.start()
+    t2.start()
+
+    elapsed_time = time.time() - start
+    print (("elapsed_time:{0}".format(elapsed_time)) , "[sec]")
